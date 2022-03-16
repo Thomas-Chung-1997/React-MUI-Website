45fbae8bf8c5d1b691ed8c8a8b109a7c682ce72f
Thomas Chung <60494228+Thomas-Chung-1997@users.noreply.github.com>
2022-03-16 16:30:35 -0600

    Added React Router Dom functionality
    Enabled NavBar links to properly redirect to different content in site using the RRD switch.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 //import "./App.css";
 import "@mui/material";
 import NavBar from "./Components/NavBar/navBar.jsx";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <div>Home</div>
+            </Route>
+            <Route path="/aboutMe">
+              <div>About Me</div>
+            </Route>
+            <Route path="/projects">
+              <div>Projects</div>
+            </Route>
+            <Route path="/contact">
+              <div>Contact</div>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Components/NavBar/navBar.jsx
+++ src/Components/NavBar/navBar.jsx
@@ -11,9 +11,7 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import { CssBaseline } from "@mui/material";
-
-// Variables
-const pages = ["Home", "About Me", "Projects", "Contact"];
+import { Link } from "react-router-dom";
 
 // NavBar component
 const NavBar = () => {
@@ -72,11 +70,51 @@ const NavBar = () => {
                 display: { xs: "block", md: "none" },
               }}
             >
-              {pages.map((page) => (
+              {/* {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
-              ))}
+              ))} */}
+              <MenuItem key="Home" onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link
+                    to="/"
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    Home
+                  </Link>
+                </Typography>
+              </MenuItem>
+              <MenuItem key="AboutMe" onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link
+                    to="/aboutMe"
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    About Me
+                  </Link>
+                </Typography>
+              </MenuItem>
+              <MenuItem key="Projects" onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link
+                    to="/projects"
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    Projects
+                  </Link>
+                </Typography>
+              </MenuItem>
+              <MenuItem key="Contact" onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link
+                    to="/contact"
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    Contact
+                  </Link>
+                </Typography>
+              </MenuItem>
             </Menu>
           </Box>
           <Typography
@@ -88,15 +126,60 @@ const NavBar = () => {
             Thomas.Chung
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
+            {/* {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: "white", display: "block" }}
               >
-                {page}
+                <Link to={"/" + page}>{page}</Link>
               </Button>
-            ))}
+            ))} */}
+            <Button
+              key="Home"
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, display: "block" }}
+            >
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                Home
+              </Link>
+            </Button>
+            <Button
+              key="About Me"
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, display: "block" }}
+            >
+              <Link
+                to="/aboutMe"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                About Me
+              </Link>
+            </Button>
+            <Button
+              key="Projects"
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, display: "block" }}
+            >
+              <Link
+                to="/projects"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Projects
+              </Link>
+            </Button>
+            <Button
+              key="Contact"
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, display: "block" }}
+            >
+              <Link
+                to="/contact"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Contact
+              </Link>
+            </Button>
           </Box>
         </Toolbar>
       </Container>
