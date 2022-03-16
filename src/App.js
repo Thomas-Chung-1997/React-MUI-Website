058548be553bc3ccb524b736c8a6191b0141b28c
Thomas Chung <60494228+Thomas-Chung-1997@users.noreply.github.com>
2022-03-16 15:02:27 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import "./App.css";
+import "@mui/material";
+import NavBar from "./Components/NavBar/navBar.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/home.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Home = () => {
+  return <div>hello</div>;
+};
+
+export default Home;
